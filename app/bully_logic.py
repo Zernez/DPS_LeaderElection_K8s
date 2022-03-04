6db30538328481bc5f2ec3b4705f9fae5e1ea66f
Zernez <fernando.pannullo@yahoo.it>
2022-03-04 23:16:37 +0100

    Work, but remain problems with go_deep() function

--- app/bully_logic.py
+++ app/bully_logic.py
@@ -15,7 +15,7 @@ class logic:
     ids_nodes= []
     hosts_ports= []
     number_of_hosts= int(os.environ["NUM_HOST"])
-    register={}
+    register= [dict() for x in range(number_of_hosts)]
     metrics = {
             "time": 0,
             "size": 0,
@@ -39,7 +39,7 @@ class logic:
         for host in self.hosts_ports:
             id_num= 0
             for port_id in self.hosts_ports:
-                service_register_status = self.register_service(host, port_id,self.ids_nodes[id_num])
+                service_register_status = self.register_service(host, port_id,self.ids_nodes[id_num], id_num)
                 id_num += 1
 
         self.start()
@@ -52,9 +52,9 @@ class logic:
 
         detail=self.get_details(self.hosts_ports)
         
-        high_ID= self.get_higher_nodes(detail,self.ID_local)
+        high_IDs= self.get_higher_nodes(detail)
 
-        winner= self.election(high_ID, self.ID_local)
+        winner= self.election(high_IDs)
 
         if winner != "Redirect":
 
@@ -66,13 +66,16 @@ class logic:
         
         return
     
-    def go_deep(self, try_port):
+    def go_deep(self):
 
-        detail=self.get_details(try_port)
+        detail=self.get_details(self.hosts_ports)
+
+        print (self.ID_local)
+        print (detail)
         
-        high_ID= self.get_higher_nodes(detail,self.ID_local)
+        high_IDs= self.get_higher_nodes(detail)
 
-        winner= self.election(high_ID, self.ID_local)
+        winner= self.election(high_IDs)
 
         if winner != "Redirect":
 
@@ -99,7 +102,7 @@ class logic:
         node_id = millis + randint(0, 20000)
         return node_id
 
-    def register_service(self, host, port_id, node_id):
+    def register_service(self, host, port_id, node_id, n):
 #        status= self.check_health_of_the_service(port_id)
 #        if status == "Failed":
 #            return status        
@@ -107,60 +110,56 @@ class logic:
             "ID": node_id,
             "port": port_id,
             "coordinator": None,
-            "election": self.election_local
+            "election": self.election_local,
+            "seq": n
         }
 
         url = self.url_local + str(host) + "/register"           
         
         if host == self.port_local:
-            self.ID_local= data["ID"]
-            print("Same port.....................", file=sys.stdout)
-            self.register.update(data)
+            self.ID_local= node_id
+            self.register[n].update(data)
             return "OK", 200
         else: 
-            print("Directed to %d....................", host, file=sys.stdout)   
             put_request = requests.post(url, json=data)              
-        
-        print(self.register, file=sys.stdout)
+
         return put_request.status_code
 
     def get_details(self, ports_of_all_nodes):
         details= []
         for host in self.register:
-            if self.register[host]['port'] in ports_of_all_nodes:
-                id = self.register[host]['ID']
-                port = self.register[host]['port']
-                election = self.register[host]['election']
+            if host['port'] != self.port_local:
+                id = host['ID']
+                port = host['port']
+                election = host['election']
                 detail = {'ID': id, 'port': port,'election': election}
                 details.append(detail)
         return details
 
-    def get_higher_nodes(self, node_details, node_id):
+    def get_higher_nodes(self, node_details):
         higher_node_array = []
         for each in node_details:
-            if each['ID'] > node_id:
+            if each['ID'] > self.ID_local:
                 higher_node_array.append(each['port'])
         return higher_node_array
 
-    def election(self, higher_nodes_array, node_id):
+    def election(self, higher_nodes_array):
         status_code_array = []
         if not higher_nodes_array:
-            return node_id
+            return self.ID_local
 
         for each_port in higher_nodes_array:
-            higher_nodes= higher_nodes_array
-            higher_nodes.remove(each_port)
             url = self.url_local + '%s/proxy' % each_port
             data = {
-                "candidate_port": higher_nodes
-            }
-            self.metrics.size+= sys.getsizeof(data)
-            self.metrics.messages+= 1
+                "candidate_port": each_port
+                }
+            self.metrics["size"]+= sys.getsizeof(data)
+            self.metrics["messages"]+= 1
             post_response = requests.post(url, json=data)
             status_code_array.append(post_response.status_code)
         
         if not 200 in status_code_array:
-            return node_id
+            return self.ID_local
              
         return "Redirect"
 
@@ -174,26 +173,33 @@ class logic:
 #        return service_status
    
     def announce(self, coordinator):
-        all_nodes = self.hosts_ports
         data = {
             'ID_coordinator': coordinator
         }
-        for each_node in all_nodes:
-            self.metrics.size+= sys.getsizeof(data)
-            self.metrics.messages+= 1
-            url = self.url_local +'%s/announce' % all_nodes[each_node]
-            requests.post(url, json=data)
-        return requests.status_code
+
+        for each_node in self.hosts_ports:
+            if each_node == self.port_local:
+                n= 0  
+                for host in self.register:
+                    self.register[n]['coordinator']= coordinator
+                    self.register[n]['election']= False
+                    n+= 1
+                self.election_local= False
+                self.coordinator_local= coordinator
+            else:
+                self.metrics["size"]+= sys.getsizeof(data)
+                self.metrics["messages"]+= 1
+                url = self.url_local +'%s/announce' % each_node
+                requests.post(url, json=data)
+        return {'Response': 'OK'}, 200
 
     def get_metrics(self):
         details = []
         for each_port in self.hosts_ports:
-            url = self.url_local + '%s/performance' % each_port
-            data = requests.get(url)
-            data.append(data.json())
-
-        for host in data:
-                self.register['time'] += data[host]['time']
-                self.register['size'] += data[host]['size']
-                self.register['messages'] += data[host]['messages']
+            if each_port != self.port_local: 
+                url = self.url_local + '%s/performance' % each_port
+                data = requests.get(url).json()          
+                self.metrics['time'] += data['time']
+                self.metrics['size'] += data['size']
+                self.metrics['messages'] += data['messages']
         return details
